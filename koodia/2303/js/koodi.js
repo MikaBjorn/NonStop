75c98ee6b3c91f4847247701751143c46766e0ba
MikaBjorn <mika.bjorn@edu.keuda.fi>
2021-09-22 14:32:18 +0300

    Koodaus Haaste 1

---
+++ koodia/2303/js/koodi.js
@@ -0,0 +1,18 @@
+<<<<<<< HEAD
+<<<<<<< HEAD
+function tervehdys() {
+alert  ('Heippa');
+alert ('maailma!');
+}
+=======
+function tervehdys() {
+alert  ('Heippa');
+alert ('maailma!');
+}
+>>>>>>> 9e1f85d66f31861bc1f4a29e59b8048ea9c0fe6f
+=======
+function tervehdys() {
+alert  ('Heippa');
+alert ('maailma!');
+}
+>>>>>>> 9e1f85d66f31861bc1f4a29e59b8048ea9c0fe6f
